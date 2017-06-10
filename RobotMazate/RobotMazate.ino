adac8f4dc013862c1d783584f211172137116d77
Antonio1394 <antoniorivm1394@gmail.com>
2017-06-09 18:04:03 -0600

    [update] switch function

--- RobotMazate/RobotMazate.ino
+++ RobotMazate/RobotMazate.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+  int var='s';
 ///variables para giro dereha
   int der=2;
   int der2=3;
@@ -7,7 +8,7 @@
   int izq2=5;
 /////variables arriba
   int arr=6;
-  int arr2=7
+  int arr2=7;
 /////variables abajo
   int aba=8;
   int aba2=9;
@@ -36,37 +37,57 @@ void loop()
       switch(var){
         case 'a':
           digitalWrite(arr,HIGH);
+          digitalWrite(arr2,HIGH);
           digitalWrite(aba,LOW);
+          digitalWrite(aba2,LOW);
           digitalWrite(izq,LOW);
+          digitalWrite(izq2,LOW);
           digitalWrite(der,LOW);
+          digitalWrite(der2,LOW);
         break;
 
         case 'b':
           digitalWrite(arr,LOW);
+          digitalWrite(arr2,LOW);
           digitalWrite(aba,HIGH);
+          digitalWrite(aba2,HIGH);
           digitalWrite(izq,LOW);
+          digitalWrite(izq2,LOW);
           digitalWrite(der,LOW);
+          digitalWrite(der2,LOW);
         break;
 
         case 'r':
           digitalWrite(arr,LOW);
+          digitalWrite(arr2,LOW);
           digitalWrite(aba,LOW);
+          digitalWrite(aba2,LOW);
           digitalWrite(izq,LOW);
+          digitalWrite(izq2,LOW);
           digitalWrite(der,HIGH);
+          digitalWrite(der2,HIGH);
         break;
 
         case 'l':
           digitalWrite(arr,LOW);
+          digitalWrite(arr2,LOW);
           digitalWrite(aba,LOW);
+          digitalWrite(aba2,LOW);
           digitalWrite(izq,HIGH);
+          digitalWrite(izq2,HIGH);
           digitalWrite(der,LOW);
+          digitalWrite(der2,LOW);
         break;
 
         default:
           digitalWrite(arr,LOW);
+          digitalWrite(arr2,LOW);
           digitalWrite(aba,LOW);
+          digitalWrite(aba2,LOW);
           digitalWrite(izq,LOW);
+          digitalWrite(izq2,LOW);
           digitalWrite(der,LOW);
+          digitalWrite(der2,LOW);
         
       }//Fin del Switch
 
